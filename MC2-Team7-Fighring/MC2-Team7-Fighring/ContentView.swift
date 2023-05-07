22870bf6136c2878a63c406e8a0f1e96158d4f6b
seunggyun-jeong <jskyun98@gmail.com>
2023-05-07 19:18:43 +0900

    [ADD] initial project

---
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MC2-Team7-Fighring
+//
+//  Created by 정승균 on 2023/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/MC2_Team7_FighringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2_Team7_FighringApp.swift
+//  MC2-Team7-Fighring
+//
+//  Created by 정승균 on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct MC2_Team7_FighringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
