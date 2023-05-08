c8d825c084f896086a2585ef2929b9426d5f902a
leeyongjun604 <daniel_604@naver.com>
2023-05-08 10:33:21 +0900

    [FEAT] LOVERDETAIL뷰 추가
    내용없이 뷰만 생성

---
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Lover/LoverDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LoverDetail.swift
+//  MC2-Team7-Fighring
+//
+//  Created by 이용준의 Macbook on 2023/05/08.
+//
+
+import SwiftUI
+
+struct LoverDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoverDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LoverDetail()
+    }
+}
