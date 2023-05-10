a3f46f91c489109b7906c36d361e9255e68cd20d
leeyongjun604 <daniel_604@naver.com>
2023-05-10 09:45:17 +0900

    [FEAT] LoverHome 수정중

--- MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Lover/LoverHome.swift
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Lover/LoverHome.swift
@@ -16,15 +16,25 @@ struct LoverHome: View {
     
     var body: some View {
         if dayCounter {
-            ScrollView() {
-                Circle()
-                    .frame(width: 240)
-                    .padding(EdgeInsets(top: 61, leading: 0, bottom: 0, trailing: 0))
+            VStack {
                 Text("\(loverName)가 보낸 메시지에여~\n확인해 보십시다")
                     .multilineTextAlignment(.center)
                     .fontWeight(.bold)
                     .font(.title)
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 61, trailing: 0))
+                    .padding(EdgeInsets(top: 149, leading: 0, bottom: 0, trailing: 0))
+                GeometryReader { geometry in
+                    Path { path in
+                        let size = geometry.size.width
+                        let nearLine = size * 0.1
+                        let farLine = size
+                        let middle = size * 0.03
+                        path.move(to: .init(x: nearLine, y: middle))
+                        path.addLine(to: .init(x: farLine, y: middle))
+                    }.stroke(Color(red: 165 / 255, green: 205 / 255, blue: 251 / 255), style: .init(lineWidth: 3.0, dash: [geometry.size.width / 20, geometry.size.width / 20], dashPhase: 0))
+                }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                EnvelopeCarouseView()
+                
+                /*
                 ForEach(0..<message.count) { messageNumber in
                     VStack {
                         HStack {
@@ -58,17 +68,16 @@ struct LoverHome: View {
                             .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
                         Spacer()
                     }.background(LinearGradient(gradient: Gradient(colors: [Color.white, Color(red: 230 / 255, green: 230 / 255, blue: 230 / 255)]), startPoint: .center, endPoint: .bottom))
+                 */
                 }
-            }
         } else {
             VStack {
-                Circle()
-                    .frame(width: 240)
-                    .padding(50)
                 Text("아직 공유받은 메시지가 없어요\n조금만 더 기다려 보아요")
                     .multilineTextAlignment(.center)
                     .fontWeight(.bold)
                     .font(.system(size: 25))
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
+                Image("not fun")
             }
         }
     }
