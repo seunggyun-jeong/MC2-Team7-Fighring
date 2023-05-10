42da77145f4a179776359561806e56b6fbc9d257
leeyongjun604 <daniel_604@naver.com>
2023-05-10 17:08:21 +0900

    [FEAT] LoverHome done

--- MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Lover/Envelope.swift
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Lover/Envelope.swift
@@ -5,24 +5,9 @@
 //  Created by 이용준의 Macbook on 2023/05/10.
 //
 
-import Foundation
+import SwiftUI
 
-struct Item_envelope: Identifiable {
-    var id: Int
-    var envelope: String
-}
-
-class EnvelopeStore: ObservableObject {
-    @Published var items: [Item_envelope]
-    
-    let envelopes: [String] = ["envelope1", "envelope2", "envelope3", "envelope4", "envelope5", "envelope6"]
-    
-    // dummy data
-    init() {
-        items = []
-        for i in 0...5 {
-            let new = Item_envelope(id: i, envelope: envelopes[i])
-            items.append(new)
-        }
-    }
+struct Envelope: Identifiable{
+    var id = UUID().uuidString
+    var envelopeImage: String
 }
--- MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Lover/EnvelopeCarouseView.swift
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Lover/EnvelopeCarouseView.swift
@@ -7,57 +7,75 @@
 
 import SwiftUI
 
-struct EnvelopeCarouseView: View {
-    @StateObject var envelopeStorage = EnvelopeStore()
-    @State private var snappedItem = 0.0
-    @State private var draggingItem = 0.0
+struct EnvelopeCarouseView<Content: View, T: Identifiable>: View {
     
-    var body: some View {
-        VStack {
-            ZStack {
-                ZStack {
-                    ForEach(envelopeStorage.items) { item in
-                        VStack {
-                            Image(item.envelope)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 220)
-                        }
-                        .scaleEffect(1.0 - abs(getDistance(item.id)) * 0.6 )
-                        .opacity(1.0 - abs(getDistance(item.id)) * 0.55 )
-                        .offset(x: getMyXOffset(item.id), y: 0)
-                        .zIndex(1.0 - abs(getDistance(item.id)) * 0.1)
-                    }
-                }.gesture(
+    
+    var content: (T) -> Content
+    var list: [T]
+    
+    var spacing: CGFloat
+    var trailingSpace: CGFloat
+    @Binding var index: Int
+    
+    init(spacing: CGFloat = 15, trailingSpace: CGFloat = 100, index: Binding<Int>, items: [T], @ViewBuilder content: @escaping (T)->Content){
+        
+        self.list = items
+        self.spacing = spacing
+        self.trailingSpace = trailingSpace
+        self._index = index
+        self.content = content
+    }
+    @GestureState var offset: CGFloat = 0
+    @State var currentIndex: Int = 0
+    
+    var body: some View{
+        
+        GeometryReader{proxy in
+            
+            
+            
+            let width = proxy.size.width - (trailingSpace - spacing)
+            let adjustMentWidth = (trailingSpace / 2) - spacing
+            
+            HStack(spacing: spacing){
+                
+                ForEach(list){item in
+                    
+                    content(item)
+                        .frame(width: proxy.size.width - trailingSpace)
+                }
+            }.padding(.horizontal, spacing)
+                .offset(x: (CGFloat(currentIndex) * -width) + (adjustMentWidth) + offset)
+                .gesture(
                     DragGesture()
-                        .onChanged { value in
-                            draggingItem = snappedItem + value.translation.width / 100
-                        }
-                        .onEnded { value in
-                            withAnimation {
-                                draggingItem = snappedItem + value.predictedEndTranslation.width / 100
-                                draggingItem = round(draggingItem).remainder(dividingBy: Double(envelopeStorage.items.count))
-                                snappedItem = draggingItem
-                            }
-                        }
-                ).padding(.bottom, 100)
-            }
+                        .updating($offset, body: { value, out, _ in
+                            out = value.translation.width
+                        })
+                        .onEnded({ value in
+                            let offsetX = value.translation.width
+                            
+                            let progress = -offsetX / width
+                            let roundIndex = progress.rounded()
+                            
+                            currentIndex = max(min(currentIndex + Int(roundIndex), list.count - 1), 0)
+                            currentIndex = index
+                        })
+                        .onChanged({value in
+                            let offsetX = value.translation.width
+                            
+                            let progress = -offsetX / width
+                            let roundIndex = progress.rounded()
+                            
+                            index = max(min(currentIndex + Int(roundIndex), list.count - 1), 0)
+                        })
+            )
         }
-    }
-    
-    func getDistance(_ item: Int) -> Double {
-        return (draggingItem - Double(item)).remainder(dividingBy: Double(envelopeStorage.items.count))
-    }
-    
-    func getMyXOffset(_ item: Int) -> Double {
-        let angle = Double.pi * 2 / Double(envelopeStorage.items.count) * getDistance(item)
-        return sin(angle) * 200
+        .animation(.easeInOut, value: offset == 0)
     }
 }
 
 struct EnvelopeCarouseView_Previews: PreviewProvider {
     static var previews: some View {
-        EnvelopeCarouseView()
+        ContentView()
     }
 }
-
