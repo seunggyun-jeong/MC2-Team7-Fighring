aae3af5c32667c9899ea21667f19b2aecdc619bb
seunggyun-jeong <jskyun98@gmail.com>
2023-05-11 12:47:46 +0900

    [FEAT] 온보딩 기능 완료

--- MC2-Team7-Fighring/MC2-Team7-Fighring/Views/ContentView.swift
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/Views/ContentView.swift
@@ -30,27 +30,28 @@ struct ContentView: View {
         if isFirst {
             OnBoardingMainView(isFirstLaunch: $isFirst)
         } else {
+            TabView(selection: $selection) {
+                MainView(questions: question)
+                    .tabItem {
+                        Label("My", systemImage: "person.circle.fill")
+                    }
+                    .tag(Tab.my)
+                
+                LoverHome()
+                    .tabItem {
+                        Label("Lover", systemImage: "heart.circle")
+                    }
+                    .tag(Tab.lover)
+                
+                ResultHome()
+                    .tabItem {
+                        Label("Result", systemImage: "book.circle.fill")
+                    }
+                    .tag(Tab.result)
+            }
 
         }
-        TabView(selection: $selection) {
-            MainView(questions: question)
-                .tabItem {
-                    Label("My", systemImage: "person.circle.fill")
-                }
-                .tag(Tab.my)
-            
-            LoverHome()
-                .tabItem {
-                    Label("Lover", systemImage: "heart.circle")
-                }
-                .tag(Tab.lover)
-            
-            ResultHome()
-                .tabItem {
-                    Label("Result", systemImage: "book.circle.fill")
-                }
-                .tag(Tab.result)
-        }
+
     }
 }
 
