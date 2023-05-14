d53b23dcc7425b29a878c2fb940267c32c44f926
JungDohyeon <jdh1109ok@naver.com>
2023-05-14 16:43:07 +0900

    [FIX]#101-tab3TestSheet

--- MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Result/TestViews/TestSheet.swift
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/Views/Result/TestViews/TestSheet.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct TestSheet: View {
-    @State var click = [Int](repeating: 0, count: 32)
-    @State var clicked = [Int](repeating: 0, count: 32)
+    @State var click = [Int](repeating: 0, count: 36)
+    @State var clicked = [Int](repeating: 0, count: 36)
     var body: some View {
         ScrollView{
-            ForEach(0 ..< 32){ y in
-                Text(QuestionList.question[y])
-                    .font(.system(size: 25))
-                    .frame(width: 325, alignment: .leading)
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 21, trailing: 0))
+            ForEach(0 ..< 36){ y in
+                
+                HStack {
+                    Text(QuestionList.question[y])
+                        .font(.system(size: 22))
+                        .padding(.bottom, 20)
+                        .padding(.trailing, 30)
+                        .padding(.top, 10)
+                        .multilineTextAlignment(.leading)
+                    
+                    Spacer()
+                    
+                }
+                .padding(.leading, 20)
+                
                 HStack(spacing: 18){
                     ForEach(1 ..< 6){ index in
                         Button(action:{
@@ -27,8 +37,8 @@ struct TestSheet: View {
                             ZStack{
                                 Circle()
                                     .frame(width: 45, height: 45)
-                                    .foregroundColor(clicked[y] == index ? .pink: .white)
-                                    .overlay(Circle().stroke(Color.gray, lineWidth: 3))
+                                    .foregroundColor(clicked[y] == index ? Color(red: 255/255, green: 151/255, blue: 172/255) : .white)
+                                    .overlay(Circle().stroke(clicked[y] == index ? Color(red: 255/255, green: 151/255, blue: 172/255) : Color.gray.opacity(0.5), lineWidth: 1))
                                 Image(systemName: "heart.fill")
                                     .foregroundColor(Color.white)
                                     .font(.system(size: 24))
@@ -37,23 +47,22 @@ struct TestSheet: View {
                         }
                     }
                 }
-                .frame(maxWidth: .infinity)
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+                .padding(.horizontal, 40)
+                .padding(.bottom, 5)
+                
                 HStack{
-                    HStack{
-                        Text("전혀 그렇지 않다")
-                            .foregroundColor(.gray)
-                            .font(.system(size: 15))
-                        Spacer()
-                        Text("매우 그렇다")
-                            .foregroundColor(.gray)
-                            .font(.system(size: 15))
-                    }
-                    .frame(width: 337)
+                    Text("전혀 그렇지 않다")
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text("매우 그렇다")
+                        .foregroundColor(.gray)
                 }
-                .frame(maxWidth: .infinity)
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0))
+                .font(.caption)
+                .padding(.leading, 30)
+                .padding(.trailing, 40)
+                .padding(.bottom, 20)
             }
+            
             Button(action: {
                 
             }){
