9aed81a4d592f30c37615ad366c9cfeb4cf55578
Seokjun Ko <tjrwns1021@gmail.com>
2023-05-17 12:13:30 +0900

    [FIX] Daily Test
    문제 높이에 따라서 동적으로 Text 그려주기

--- MC2-Team7-Fighring/MC2-Team7-Fighring/Views/My/DailyTest.swift
+++ MC2-Team7-Fighring/MC2-Team7-Fighring/Views/My/DailyTest.swift
@@ -56,11 +56,13 @@ struct DailyTest: View {
                 .padding(.bottom, 42)
                 
                 // 질문
-                    Text("\(QuestionList.question[Int(questionData.questionNum)-1])")
-                        .font(.system(size: 24
-                                     ))
-                        .fontWeight(.medium)
-                        .frame(height: QuestionList.question[Int(questionData.questionNum)-1].count < 35 ? 80 : 130)
+                Text("\(QuestionList.question[Int(questionData.questionNum)-1])")
+                    .font(.system(size: 24))
+                    .fontWeight(.medium)
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(alignment: .leading)
+                    .frame(maxWidth: .infinity)
             }
             .padding(.top, 42)
             .padding(.bottom, 34)
